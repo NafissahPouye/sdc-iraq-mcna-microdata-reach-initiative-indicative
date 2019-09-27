cf435ced167a1badaf358a3f2830460875cd30de
Nafissah Pouye <Nafissah Pouye>
2019-09-27 11:44:10 +0000

    initial commit

---
+++ code.R
@@ -0,0 +1,39 @@
+#Load required libraries
+library(readxl)      #for excel, csv sheets manipulation
+library(sdcMicro)    #sdcMicro package with functions for the SDC process 
+library(tidyverse)   #optional #for data cleaning
+
+#Import data
+setwd("C:/Users/LENOVO T46OS/Desktop/sdc-iraq-mcna-microdata-reach-initiative_representative_indicative")
+data <- read_excel("data.xlsx", sheet = "hh_data_indicative")
+
+
+#Select key variables                   
+selectedKeyVars <- c('population_group', 'district','hhh', 'age_respondent',
+                     'gender_respondent','num_hh_member')
+
+#select weights
+weightVars <- c('weights')
+
+#Convert variables into factors
+cols =  c('population_group', 'district','hhh', 'age_respondent',
+          'gender_respondent','num_hh_member')
+
+data[,cols] <- lapply(data[,cols], factor)
+
+#Convert the sub file into a dataframe
+subVars <- c(selectedKeyVars, weightVars)
+fileRes<-data[,subVars]
+fileRes <- as.data.frame(fileRes)
+objSDC <- createSdcObj(dat = fileRes, 
+                       keyVars = selectedKeyVars, weightVar = weightVars
+                       )
+
+#print the risk
+print(objSDC, "risk")
+max(objSDC@risk$global[, "risk"])
+
+#Generate an internal (extensive) report
+report(objSDC, filename = "index",internal = T, verbose = TRUE) 
+
+
